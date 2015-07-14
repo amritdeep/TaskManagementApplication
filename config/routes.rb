1b5ad1ba628bf5c34a77e7976b3c28a99acf3556
AmritDeep <amritdeepdhungana@hotmail.com>
2015-07-14 17:54:40 +0545

    Generate Home Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   devise_for :users
   root to: "home#index"
   # The priority is based upon order of creation: first created -> highest priority.
