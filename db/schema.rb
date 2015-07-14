4926b184f2dca974b37909833e48875b2866855d
AmritDeep <amritdeepdhungana@hotmail.com>
2015-07-14 18:20:47 +0545

    generate task model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150714120725) do
+ActiveRecord::Schema.define(version: 20150714123519) do
+
+  create_table "tasks", force: true do |t|
+    t.string   "title"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
