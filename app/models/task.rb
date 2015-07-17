9c19bed37a8a7dce23d1c9d29114efc8bf7dc320
AmritDeep <amritdeepdhungana@hotmail.com>
2015-07-18 00:38:08 +0545

    Show has many and belongs to relationship

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,12 @@
 class Task < ActiveRecord::Base
-  belongs_to :user
+  # belongs_to :user
+
+  has_and_belongs_to_many :users
 
   # scope :desc, order(created_at: :desc)
+
+  def self.user_name(id)
+  	User.where(id: id)
+  end
+  
 end
