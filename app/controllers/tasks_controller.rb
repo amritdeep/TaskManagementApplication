140cacb23072b4420b4b7fbe6ccdb93387603cd1
AmritDeep <amritdeepdhungana@hotmail.com>
2015-07-17 20:00:33 +0545

    Create show page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
 	before_action :authenticate_user!
 
 	def index
-		@tasks = Task.all
+		@tasks = Task.all.order(created_at: :desc)
 	end
 
 	def new
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,5 @@
 class Task < ActiveRecord::Base
   belongs_to :user
+
+  # scope :desc, order(created_at: :desc)
 end
